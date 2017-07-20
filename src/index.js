8e6ba6281044e7b8cfb050532db17a42fad7fb22
Ellis Clayton <ellis@ellisclayton.info>
2017-07-21 08:27:20 +1000

    Arrays are better

--- src/index.js
+++ src/index.js
@@ -9,49 +9,63 @@ type PrismicQueryResponse = {
 
 type DocumentCount = {
   count: number,
-  type: string
+  type: string,
+  description: string,
 }
 
-const documentTypes = {
-  'days-since-activated': 'Phase 2 (Days since page activated)',
-  'dollar-milestone': 'Phase 6 (Donation value milestone)',
-  'donation-received': 'Transactional (Donation received)',
-  'fitness-distance-milestone': 'Phase 6 (Fitness distance milestone)',
-  'inactive-page': 'BAT (Inactive page)',
-  'milestone-percentage': 'Phase 6 (Donation percentage milestone)',
-  'page-activation': 'Phase 1 (Days since page activated)'
-}
+const documentTypes: Array<{type: string, description: string}> = [{
+  type: 'days-since-activated',
+  description: 'Phase 2 (Days since page activated)'
+}, {
+  type: 'dollar-milestone',
+  description: 'Phase 6 (Donation value milestone)'
+}, {
+  type: 'donation-received',
+  description: 'Transactional (Donation received)'
+}, {
+  type: 'fitness-distance-milestone',
+  description: 'Phase 6 (Fitness distance milestone)'
+}, {
+  type: 'inactive-page',
+  description: 'BAT (Inactive page)'
+}, {
+  type: 'milestone-percentage',
+  description: 'Phase 6 (Donation percentage milestone)'
+}, {
+  type: 'page-activation',
+  description: 'Phase 1 (Days since page activated)'
+}]
 
-const fetchDocumentCount = (documentType) => (
+const fetchDocumentCount = ({ type, description }) => (
   prismic
   .getApi(process.env.PRISMIC_API_URL, {accessToken: process.env.PRISMIC_API_KEY})
   .then((api) => {
     return api.query(
-      prismic.Predicates.at('document.type', documentType),
+      prismic.Predicates.at('document.type', type),
       { pageSize: 1 }
     )
   }).then(({ total_results_size: count }: PrismicQueryResponse) => ({
     count,
-    type: documentType
-  }), (err) => (
-    Promise.reject(new Error(err))
-  ))
+    type,
+    description
+  })).catch(console.log || process.exit(1))
 )
 
 Promise.all(
-  keys(documentTypes)
-  .map(fetchDocumentCount)
-).then((res: [DocumentCount]) => {
+  documentTypes.map(fetchDocumentCount)
+).then((res: Array<DocumentCount>) => {
   const total = res.reduce((acc: number, { count }) => (
     acc + count
   ), 0)
 
   console.log(table.print(
-    res.map(({ count, type }) => ({
+    res
+    .sort((a, b) => (b.count - a.count))
+    .map(({ count, type, description }) => ({
       count,
       percentage: Math.round(count / total * 100),
-      description: documentTypes[type]
-    })).sort((a, b) => (b.count - a.count))
+      description: description
+    }))
   ))
 
   console.log(`${total} active documents total`)
